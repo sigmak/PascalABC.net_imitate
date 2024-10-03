3e5fcee37b89bb3324abe38a6f5dbd7969caa8a6
sigmak <dwfree74@gmail.com>
2024-10-04 02:44:32 +0900

    Ver-0.2.2
    About 폼 완료. 너무 복잡한것만 simple 하게 변경...ㅜ.ㅠ;

--- uAboutBox.pas
+++ uAboutBox.pas
@@ -2,19 +2,31 @@
 
 interface
 
-uses System, System.Drawing, System.Windows.Forms;
+uses System, System.Drawing, System.Windows.Forms, System.Diagnostics;
 
 type
   TAboutBox = class(Form)
-    procedure About_Load(sender: Object; e: EventArgs);
+    groupBox1: GroupBox;
+    dgvModules: ListView;
+    chType, chName, chVersion, chCopyright: ColumnHeader;
+    button1: Button;
+    linkLabel1, linkLabel2, linkLabel3, linkLabel4: LinkLabel;
+    lVersion, label12, label9, lVersionText, label1, label4, label5, label2, label3, label6, label13: &Label;
+    tableLayoutPanel1: TableLayoutPanel;
+    flowLayoutPanel1, flowLayoutPanel2, flowLayoutPanel3, flowLayoutPanel4: FlowLayoutPanel;
+    groupBox2: GroupBox;  
+    
+    procedure AboutBox_Load(sender: Object; e: EventArgs);
+    procedure AboutBox_Shown(sender: Object; e: EventArgs);
     procedure button1_Click(sender: Object; e: EventArgs);
+    procedure groupBox1_Enter(sender: Object; e: EventArgs);    
+    procedure linkLabel1_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+    procedure linkLabel2_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+    procedure linkLabel3_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+    procedure linkLabel4_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
   {$region FormDesigner}
   internal
     {$resource uAboutBox.TAboutBox.resources}
-    button1: Button;
-    listView1: ListView;
-    groupBox2: GroupBox;
-    groupBox1: GroupBox;
     {$include uAboutBox.TAboutBox.inc}
   {$endregion FormDesigner}
   public
@@ -22,15 +34,13 @@ type
     begin
       InitializeComponent;
     end;
-   
+{  // 여기에  uAboutBox.TAboutBox.inc 의 procedure InitializeComponent; 프로시저 옮겨서 디버깅 
+   //  ..한쪽은 주석으로 막아야 중복 선언 오류 피할 수 있음
+
+}
   end;
 
 implementation
-
-var
-  dgvModules: ListView;
-  chType, chName, chVersion, chCopyright: ColumnHeader;
-
 
 // MakeItem 함수 정의 (ListViewItem을 생성)
 function MakeItem(item1, item2, item3, item4: string): ListViewItem;
@@ -45,65 +55,46 @@ begin
   Result := listItem;
 end;
 
-procedure TAboutBox.About_Load(sender: Object; e: EventArgs);
+function AssemblyCopyright(a: System.Reflection.Assembly): string;
+var
+  attributes: array of object;
 begin
-  self.Text := 'About PascalABC.NET';  // 현재 폼의 Text 속성 설정
-  self.Height := 345 + 14;
-  self.Width := 546 + 15 + 94;
-  self.ShowIcon := false; // 폼 아이콘 안보이게 설정
-  self.MaximizeBox := false;
-  self.MinimizeBox := false;
+  // 어셈블리에서 AssemblyCopyrightAttribute 특성 가져오기
+  attributes := a.GetCustomAttributes(typeof(System.Reflection.AssemblyCopyrightAttribute), False);
   
-  groupBox1.Left := 8;
-  groupBox1.Top := 160 - 10;
-  groupBox1.Width := 530 + 50 + 40;
-  groupBox1.Height := 137 - 10;
-  groupBox1.Text := 'Components';
- 
+  // Copyright 특성이 없으면 빈 문자열 반환
+  if Length(attributes) = 0 then
+    Result := ''
+  else
+    // Copyright 특성이 있으면 그 값을 반환
+    Result := (System.Reflection.AssemblyCopyrightAttribute(attributes[0])).Copyright;
+end;
 
-  button1.Top := groupBox1.Top + groupBox1.Height + 10;
-  button1.Left := 308 - 50;
+
+function MakeItemA(a: System.Reflection.Assembly): ListViewItem;
+begin
+  Result := MakeItem('Assembly', 
+                      System.IO.Path.GetFileName(a.ManifestModule.FullyQualifiedName), 
+                      a.GetName().Version.ToString(), 
+                      AssemblyCopyright(a));
+end;
+
+
+procedure TAboutBox.AboutBox_Shown(sender: Object; e: EventArgs);
+var
+  s0, s1, s2 : string;
+  appPath: string;
+  dllPath: string;
+  assembly: System.Reflection.Assembly;  
   
-  button1.Width := 107;
-  button1.Height := 23;
-  button1.Text := 'OK';
-  button1.UseVisualStyleBackColor := true;
-  
-  dgvModules := listview1;
-  dgvModules.AllowColumnReorder := true;    // 컬럼 재정렬 허용
-  dgvModules.AutoArrange := false;          // 자동 정렬 비활성화
-  dgvModules.FullRowSelect := true;         // 전체 행 선택
-  dgvModules.GridLines := true;             // 그리드 라인 표시
-  dgvModules.HeaderStyle := ColumnHeaderStyle.Nonclickable; // 헤더 클릭 비활성화
-  dgvModules.HideSelection := false;        // 선택 해제 시에도 선택 상태 유지
-  dgvModules.MultiSelect := false;          // 다중 선택 비활성화
-  dgvModules.Dock := DockStyle.Fill;        // ListView를 전체 화면에 맞춤
-  dgvModules.View := View.Details;          // Details 뷰로 설정
-  // 컬럼 헤더 생성
-  chType := new ColumnHeader;
-  chType.Text := 'Type';
-  chType.Width := 65; //100;
+begin
+  s0 := '3.10';
+  s1 := '3547';
+  s2 := '2024-09-25';
+  lVersion.Text := Format('{0}, сборка {1} ({2})', s0, s1, s2);//'3.10, сборка 3547 (2024-09-25)';
+  //lVersion.Text = string.Format("{0}, сборка {1} ({2})", PascalABCCompiler.Compiler.ShortVersion, RevisionClass.Revision, PascalABCCompiler.Compiler.VersionDateTime.ToShortDateString());
+  dgvModules.Items.Clear();
 
-  chName := new ColumnHeader;
-  chName.Text := 'Name';
-  chName.Width := 170; //150;
-
-  chVersion := new ColumnHeader;
-  chVersion.Text := 'Version';
-  chVersion.Width := 74; //100;
-
-  chCopyright := new ColumnHeader;
-  chCopyright.Text := 'Copyright';
-  chCopyright.Width := 200; //150;
-
-  // 컬럼 헤더 추가
-  //dgvModules.Columns.AddRange([chType, chName, chVersion, chCopyright]); //오류발생
-  // 컬럼 헤더 개별적으로 추가
-  dgvModules.Columns.Add(chType);
-  dgvModules.Columns.Add(chName);
-  dgvModules.Columns.Add(chVersion);
-  dgvModules.Columns.Add(chCopyright);  
-  
   // ListView 항목 추가
   dgvModules.Items.Add(MakeItem('Core', 'PascalABCCompiler.Core', '3.10.0.3543', 'Copyright © 2005-2024 by Ivan Bondarev, Stanislav Mikhalkovich'));  
   dgvModules.Items.Add(MakeItem('Language', 'PascalABC.NET', '1.2', 'Copyright ⓒ 2005-2024 by Ivan Bondarev, Stanislav Mikhalkovich'));  
@@ -112,14 +103,93 @@ begin
   dgvModules.Items.Add(MakeItem('Plugin', 'InternalError Report Genegator', '1.2', 'Copyright ⓒ 2005-2024 by Ivan Bondarev, Stanislav Mikhalkovich'));  
   dgvModules.Items.Add(MakeItem('Plugin', 'PT4 Provider', '1.1', 'Copyright ⓒ 2005-2024 by Ivan Bondarev, Stanislav Mikhalkovich'));  
   dgvModules.Items.Add(MakeItem('Plugin', 'Teacher Control Plugin', '0.1', 'Copyright ⓒ 2021-2024 by Stanislav Mikhalkovich'));  
-  dgvModules.Items.Add(MakeItem('Assembly', 'ICSharpCode.TextEditor.dll', '0.0.0.0', ''));  
-  dgvModules.Items.Add(MakeItem('Assembly', 'Debugger.Core.dll', '3.0.0.2649', '2000-2007 AlphaSierraPapa'));  
-  dgvModules.Items.Add(MakeItem('Assembly', 'WeifenLuo.WinFormsUI.Docking.dll', '2.2.7423.21162', 'Copyright ⓒ Weifen Luo 2007'));  
+
+  
+  // 경로 조합
+  appPath := GetCurrentDir; // 또는 특정 경로 지정
+
+  try
+    
+    dllPath := System.IO.Path.Combine(appPath, 'ICSharpCode.TextEditor.dll');
+    // DLL 로드
+    assembly := System.Reflection.Assembly.LoadFile(dllPath);
+    writeln('DLL 로드 성공: ', assembly.FullName);
+    dgvModules.Items.Add(MakeItemA(assembly));  
+    //dgvModules.Items.Add(MakeItem('Assembly', 'ICSharpCode.TextEditor.dll', '0.0.0.0', ''));  
+    //dgvModules.Items.Add(MakeItem(Assembly.LoadFile(Path.Combine(apppatch,"ICSharpCode.TextEditor.dll"))));
+    
+    dllPath := System.IO.Path.Combine(appPath, 'Debugger.Core.dll');
+    // DLL 로드
+    assembly := System.Reflection.Assembly.LoadFile(dllPath);
+    writeln('DLL 로드 성공: ', assembly.FullName);
+    dgvModules.Items.Add(MakeItemA(assembly));  
+    //  dgvModules.Items.Add(MakeItem('Assembly', 'Debugger.Core.dll', '3.0.0.2649', '2000-2007 AlphaSierraPapa'));  
+    //dgvModules.Items.Add(MakeItem(Assembly.LoadFile(Path.Combine(apppatch,"Debugger.Core.dll"))));
+
+    dllPath := System.IO.Path.Combine(appPath, 'WeifenLuo.WinFormsUI.Docking.dll');
+    // DLL 로드
+    assembly := System.Reflection.Assembly.LoadFile(dllPath);
+    writeln('DLL 로드 성공: ', assembly.FullName);
+    dgvModules.Items.Add(MakeItemA(assembly));  
+    // dgvModules.Items.Add(MakeItem('Assembly', 'WeifenLuo.WinFormsUI.Docking.dll', '2.2.7423.21162', 'Copyright ⓒ Weifen Luo 2007'));  
+    //dgvModules.Items.Add(MakeItem(Assembly.LoadFile(Path.Combine(apppatch,"WeifenLuo.WinFormsUI.Docking.dll"))));
+
+  except
+      writeln('DLL 파일을 찾을 수 없습니다');
+    end;  
+    ActiveControl := button1;
+  
   
 end;
 
+procedure TAboutBox.AboutBox_Load(sender: Object; e: EventArgs);
+begin
+  self.Text := 'About PascalABC.NET';  // 현재 폼의 Text 속성 설정
+  self.Height := 345 + 10;//345 + 14;
+  self.Width := 546 + 94 - 15 - 10;//546 + 15 + 94;
+  self.ShowIcon := false; // 폼 아이콘 안보이게 설정
+  self.MaximizeBox := false;
+  self.MinimizeBox := false;
+  
+  
+end;
 
+procedure TAboutBox.groupBox1_Enter(sender: Object; e: EventArgs);
+begin
+  // 여기에 필요한 코드 작성
+end;
 
+procedure TAboutBox.linkLabel1_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+var
+  url: string;
+begin
+  url := 'https://pascalabc.net/';//PascalABCCompiler.StringResources.Get('!PASCALABCNET_SITE_LINK');
+  Process.Start(url);
+end;
+
+procedure TAboutBox.linkLabel2_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+var
+  url: string;
+begin
+  url := 'https://forum.mmcs.sfedu.ru/c/pascalabcnet/';//PascalABCCompiler.StringResources.Get("!PASCALABCNET_FORUM_LINK");
+  Process.Start(url);
+end;
+
+procedure TAboutBox.linkLabel3_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+var
+  url: string;
+begin
+  url := 'https://pascalabc.net/razrabotchiki-pascalabc-net/';//PascalABCCompiler.StringResources.Get("!PASCALABCNET_DEVELOPERS_LINK");
+  Process.Start(url);
+end;
+
+procedure TAboutBox.linkLabel4_LinkClicked(sender: Object;  e: LinkLabelLinkClickedEventArgs);
+var
+  url: string;
+begin
+  url := 'https://t.me/pascalabc_official/';//PascalABCCompiler.StringResources.Get("!PASCALABCNET_TELEGRAM_CHANNEL_LINK");
+  Process.Start(url);
+end;
 procedure TAboutBox.button1_Click(sender: Object; e: EventArgs);
 begin
   self.Close; // 폼닫기 명령어
---
+++ uTest.pas
@@ -0,0 +1,31 @@
+﻿Unit uTest;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  Form1 = class(Form)
+    procedure Form1_Load(sender: Object; e: EventArgs);
+  {$region FormDesigner}
+  internal
+    {$resource uTest.Form1.resources}
+    tableLayoutPanel1: TableLayoutPanel;
+    label1: &Label;
+    {$include uTest.Form1.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+procedure Form1.Form1_Load(sender: Object; e: EventArgs);
+begin
+  label1.Font := new FontFamily('굴림');
+end;
+
+end.
